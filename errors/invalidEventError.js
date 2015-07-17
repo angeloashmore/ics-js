2eba05d42a66c955ddf8fb4c4983a138dbb5ebb4
Angelo Ashmore <angeloashmore@me.com>
2015-07-17 05:33:04 -1000

    Repo cleanup

--- errors/invalidEventError.js
+++
@@ -1,6 +0,0 @@
-export default function InvalidEventError(message) {
-  this.name = "InvalidEventError";
-  this.message = message || "Event is not configured correctly.";
-}
-InvalidEventError.prototype = Object.create(Error.prototype);
-InvalidEventError.prototype.constructor = InvalidEventError;
--- errors/noEventsError.js
+++
@@ -1,6 +0,0 @@
-export default function NoEventsError(message) {
-  this.name = "NoEventsError";
-  this.message = message || "No events added.";
-}
-NoEventsError.prototype = Object.create(Error.prototype);
-NoEventsError.prototype.constructor = NoEventsError;

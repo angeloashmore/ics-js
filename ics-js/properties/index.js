5756d9815ea46e6afb1bcb39604c37f448973ab9
Angelo Ashmore <angeloashmore@me.com>
2015-07-19 01:02:01 -1000

    Adding VALARM and props

--- ics-js/properties/index.js
+++ ics-js/properties/index.js
@@ -1,19 +1,25 @@
+import ACTION from "./ACTION";
+import DESCRIPTION from "./DESCRIPTION";
 import DTSTAMP from "./DTSTAMP";
 import DTSTART from "./DTSTART";
 import DTEND from "./DTEND";
 import LOCATION from "./LOCATION";
 import PRODID from "./PRODID";
 import SUMMARY from "./SUMMARY";
+import TRIGGER from "./TRIGGER";
 import UID from "./UID";
 import VERSION from "./VERSION";
 
 export default {
+  ACTION,
+  DESCRIPTION,
   DTSTAMP,
   DTSTART,
   DTEND,
   LOCATION,
   PRODID,
   SUMMARY,
+  TRIGGER,
   UID,
   VERSION
 }
